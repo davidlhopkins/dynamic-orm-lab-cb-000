372cd365a5fe8389bca4da6826e7c9916e6bebd3
David Hopkins <dlhopkins2@gmail.com>
2018-07-03 20:07:12 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,9 @@ def self.find_by_name(name)
   DB[:conn].execute(sql)
 end
 
+def self.find_by(attribute)
+  sql = "SELECT * FROM #{self.table_name} WHERE attribute = '#{attribute}'"
+  DB[:conn].execute(sql)
+end
+
 end
